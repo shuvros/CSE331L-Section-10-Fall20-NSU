e1f8d27a995699925f45d60f565b11c78a9f0c60
shadman sameer <macbook@MacBooks-MacBook-Air.local>
2020-11-17 20:54:37 +0600

    homework complete

---
+++ Homework Submission/Homework 1/01.asm
@@ -0,0 +1,20 @@
+
+org 100h
+
+
+
+
+ADD AX, 1
+ 
+ADD AX, 2
+ 
+ADD AX, 4
+ 
+
+MOV CX, AX
+
+ret
+
+
+
+
---
+++ Homework Submission/Homework 1/02.asm
@@ -0,0 +1,22 @@
+
+
+org 100h
+
+
+ADD AX, 2
+ 
+ADD AX, 4
+ 
+ADD AX, 6
+
+ADD AX, 8
+
+ADD AX, 10
+ 
+
+MOV CX, AX
+
+ret
+
+
+
